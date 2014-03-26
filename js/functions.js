f4ee30d555b787585797f7132ff38882bb9b0c90
Khalid <Khalid.s.almuraee@gmail.com>
2014-03-27 01:10:06 +0300

    Update 2
    Uses JS in external File to improve readability

---
+++ js/functions.js
@@ -0,0 +1,64 @@
+
+<SCRIPT>
+
+		//var c1=document.getElementById("H1");
+		//var strUser=c1.options[e.selectedIndex].value;
+		//var strUser2=c1.options[e.selectedIndex].text;
+
+//document.getElementById("oh").innerHTML=c1+" and "+strUser+"and"strUser2;
+
+
+//Equation (Grade * hours)+(Grade * Hours)/Total sum of hours
+
+
+
+var get_id = new Array();
+
+
+		function changeVal(){
+			for(var i=1;i<b1.length;i++){
+				for(var j=1;j<=2;j++){
+			//var get_id= document.getElementById(b1);
+			get_id[i]=document.getElementById('b'+i);
+			alert(get_id[i][get_id[i].selectedIndex].value);
+		}
+	}
+
+	function clear(){
+
+		document.getElementById("mySelect").selectedIndex="0";
+	}
+
+	function calculation(){
+
+
+
+
+
+
+
+		
+		};
+	
+
+		function claculate()
+		{
+			for (var j=0;j<get_id.length;j++){
+			document.getElementById("Result").innerHTML="Your Overall GPA is:"+"\n";
+	}
+
+		}
+
+		function clear() {
+		
+		    if (document.selection) 
+              document.selection.clear ();
+		}
+
+
+
+		}
+	
+//document.getElementByID("Calc");
+
+		</SCRIPT>
